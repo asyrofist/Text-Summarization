8390f9efd6fad2eb00ef7ef24f1c3dd49c840d2d
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-20 19:45:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-
 import re
 import string
 import numpy as np
 import matplotlib.pyplot as plt
+import nltk
 from nltk import pos_tag
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -10,3 +10,5 @@ from nltk.corpus.reader.wordnet import NOUN, VERB, ADJ, ADV
 from nltk.corpus import brown, stopwords
 from nltk.cluster.util import cosine_distance
 from operator import itemgetter
+
+nltk.download('brown')
