61575c570001ae75a2c68bab10f262d368798df2
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-20 19:57:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,3 +22,141 @@ st.header("Summarization Corpus Brown")
 sentences = brown.sents('ca01')
 list_sentences = [' '.join(sent) for sent in sentences]
 st.write(list_sentences)
+
+class TextCleaner():
+    
+    def __init__(self):
+        self.stop_words = set(stopwords.words("english"))
+        self.punctuations = set(string.punctuation)
+        self.pos_tags = {
+                NOUN: ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'PRP$', 'WP', 'WP$'],
+                VERB: ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'],
+                ADJ: ['JJ', 'JJR', 'JJS'],
+                ADV: ['RB', 'RBR', 'RBS', 'WRB']
+        }
+
+
+    def _remove_stop_words(self, words):
+        return [w for w in words if w not in self.stop_words]
+     
+    
+    def _remove_regex(self):
+        self.input_sent = " ".join([w.lower() for w in self.input_sent])
+        self.input_sent = re.sub(r"i'm", "i am", self.input_sent)
+        self.input_sent = re.sub(r"he's", "he is", self.input_sent)
+        self.input_sent = re.sub(r"she's", "she is", self.input_sent)
+        self.input_sent = re.sub(r"that's", "that is", self.input_sent)
+        self.input_sent = re.sub(r"what's", "what is", self.input_sent)
+        self.input_sent = re.sub(r"where's", "where is", self.input_sent)
+        self.input_sent = re.sub(r"\'ll", " will", self.input_sent)
+        self.input_sent = re.sub(r"\'ve", " have", self.input_sent)
+        self.input_sent = re.sub(r"\'re", " are", self.input_sent)
+        self.input_sent = re.sub(r"\'d", " would", self.input_sent)
+        self.input_sent = re.sub(r"won't", "will not", self.input_sent)
+        self.input_sent = re.sub(r"can't", "cannot", self.input_sent)
+        self.input_sent = re.sub(r"don't", "do not", self.input_sent)
+        patterns = re.finditer("#[\w]*", self.input_sent)
+        for pattern in patterns:
+            self.input_sent = re.sub(pattern.group().strip(), "", self.input_sent)
+        self.input_sent = "".join(ch for ch in self.input_sent if ch not in self.punctuations)
+    
+    
+    def _tokenize(self):
+        return word_tokenize(self.input_sent)
+    
+    
+    def _process_content_for_pos(self, words):
+        tagged_words = pos_tag(words)
+        pos_words = []
+        for word in tagged_words:
+            flag = False
+            for key, value in self.pos_tags.items():
+                if word[1] in value:
+                    pos_words.append((word[0], key))
+                    flag = True
+                    break
+            if not flag:
+                pos_words.append((word[0], NOUN))
+        return pos_words
+       
+                 
+    def _remove_noise(self):
+        self._remove_regex()
+        words = self._tokenize()
+        noise_free_words = self._remove_stop_words(words)
+        return noise_free_words
+    
+    
+    def _normalize_text(self, words):
+        lem = WordNetLemmatizer()
+        pos_words = self._process_content_for_pos(words)
+        normalized_words = [lem.lemmatize(w, pos=p) for w, p in pos_words]
+        return normalized_words
+    
+    
+    def clean_up(self, input_sent):
+        self.input_sent = input_sent
+        cleaned_words = self._remove_noise()
+        cleaned_words = self._normalize_text(cleaned_words)
+        return cleaned_words
+      
+def pagerank(M, eps=1.0e-8, d=0.85):
+    N = M.shape[1]
+    v = np.random.rand(N, 1)
+    v = v / np.linalg.norm(v, 1)
+    last_v = np.ones((N, 1), dtype=np.float32) * np.inf
+    M_hat = (d * M) + (((1 - d) / N) * np.ones((N, N), dtype=np.float32))
+    
+    while np.linalg.norm(v - last_v, 2) > eps:
+        last_v = v
+        v = np.matmul(M_hat, v)
+    return v
+  
+def sentence_similarity(sent1, sent2):
+    text_cleaner = TextCleaner()
+    
+    sent1 = text_cleaner.clean_up(sent1)
+    sent2 = text_cleaner.clean_up(sent2)
+    
+    all_words = list(set(sent1 + sent2))
+    
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+    
+    for w in sent1:
+        vector1[all_words.index(w)] += 1
+    
+    for w in sent2:
+        vector2[all_words.index(w)] += 1
+    
+    return 1 - cosine_distance(vector1, vector2)
+  
+def build_similarity_matrix(sentences):
+    S = np.zeros((len(sentences), len(sentences)))
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            if i == j:
+                continue
+            else:
+                S[i][j] = sentence_similarity(sentences[i], sentences[j])
+    
+    for i in range(len(S)):
+        S[i] /= S[i].sum()
+    return S
+  
+st.subheader("Sentence Ranking")
+S = build_similarity_matrix(sentences)
+st.write(S)
+sentence_ranks = pagerank(S)
+st.write(sentence_ranks)
+ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
+st.write(ranked_sentence_indexes)
+
+SUMMARY_SIZE = 5
+selected_sentences = sorted(ranked_sentence_indexes[:SUMMARY_SIZE])
+st.write(selected_sentences)
+
+st.subheader("Summary Result")
+summary = itemgetter(*selected_sentences)(sentences)
+for sent in summary:
+    st.write(' '.join(sent))
