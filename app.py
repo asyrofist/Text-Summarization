8ef6dcc0671db3ca33ec11589111f658e79665e8
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-20 20:09:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,102 +4,19 @@ import string
 import numpy as np
 import matplotlib.pyplot as plt
 import nltk
-from nltk import pos_tag
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus.reader.wordnet import NOUN, VERB, ADJ, ADV
-from nltk.corpus import brown, stopwords
+from nltk.corpus import brown
 from nltk.cluster.util import cosine_distance
 from operator import itemgetter
+from function import TextCleaner
 
 nltk.download('brown')
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('wordnet')
 
 st.header("Summarization Corpus Brown")
 sentences = brown.sents('ca01')
 list_sentences = [' '.join(sent) for sent in sentences]
-st.write(list_sentences)
+st.dataframe(list_sentences)
 
-class TextCleaner():
-    
-    def __init__(self):
-        self.stop_words = set(stopwords.words("english"))
-        self.punctuations = set(string.punctuation)
-        self.pos_tags = {
-                NOUN: ['NN', 'NNS', 'NNP', 'NNPS', 'PRP', 'PRP$', 'WP', 'WP$'],
-                VERB: ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ'],
-                ADJ: ['JJ', 'JJR', 'JJS'],
-                ADV: ['RB', 'RBR', 'RBS', 'WRB']
-        }
-
-
-    def _remove_stop_words(self, words):
-        return [w for w in words if w not in self.stop_words]
-     
-    
-    def _remove_regex(self):
-        self.input_sent = " ".join([w.lower() for w in self.input_sent])
-        self.input_sent = re.sub(r"i'm", "i am", self.input_sent)
-        self.input_sent = re.sub(r"he's", "he is", self.input_sent)
-        self.input_sent = re.sub(r"she's", "she is", self.input_sent)
-        self.input_sent = re.sub(r"that's", "that is", self.input_sent)
-        self.input_sent = re.sub(r"what's", "what is", self.input_sent)
-        self.input_sent = re.sub(r"where's", "where is", self.input_sent)
-        self.input_sent = re.sub(r"\'ll", " will", self.input_sent)
-        self.input_sent = re.sub(r"\'ve", " have", self.input_sent)
-        self.input_sent = re.sub(r"\'re", " are", self.input_sent)
-        self.input_sent = re.sub(r"\'d", " would", self.input_sent)
-        self.input_sent = re.sub(r"won't", "will not", self.input_sent)
-        self.input_sent = re.sub(r"can't", "cannot", self.input_sent)
-        self.input_sent = re.sub(r"don't", "do not", self.input_sent)
-        patterns = re.finditer("#[\w]*", self.input_sent)
-        for pattern in patterns:
-            self.input_sent = re.sub(pattern.group().strip(), "", self.input_sent)
-        self.input_sent = "".join(ch for ch in self.input_sent if ch not in self.punctuations)
-    
-    
-    def _tokenize(self):
-        return word_tokenize(self.input_sent)
-    
-    
-    def _process_content_for_pos(self, words):
-        tagged_words = pos_tag(words)
-        pos_words = []
-        for word in tagged_words:
-            flag = False
-            for key, value in self.pos_tags.items():
-                if word[1] in value:
-                    pos_words.append((word[0], key))
-                    flag = True
-                    break
-            if not flag:
-                pos_words.append((word[0], NOUN))
-        return pos_words
-       
-                 
-    def _remove_noise(self):
-        self._remove_regex()
-        words = self._tokenize()
-        noise_free_words = self._remove_stop_words(words)
-        return noise_free_words
-    
-    
-    def _normalize_text(self, words):
-        lem = WordNetLemmatizer()
-        pos_words = self._process_content_for_pos(words)
-        normalized_words = [lem.lemmatize(w, pos=p) for w, p in pos_words]
-        return normalized_words
-    
-    
-    def clean_up(self, input_sent):
-        self.input_sent = input_sent
-        cleaned_words = self._remove_noise()
-        cleaned_words = self._normalize_text(cleaned_words)
-        return cleaned_words
-      
+# Function
 def pagerank(M, eps=1.0e-8, d=0.85):
     N = M.shape[1]
     v = np.random.rand(N, 1)
@@ -145,16 +62,20 @@ def build_similarity_matrix(sentences):
     return S
   
 st.subheader("Sentence Ranking")
+col1, col2 = st.beta_columns([3, 1])
 S = build_similarity_matrix(sentences)
-st.write(S)
+col1.write(S)
 sentence_ranks = pagerank(S)
-st.write(sentence_ranks)
-ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
-st.write(ranked_sentence_indexes)
+col2.write(sentence_ranks)
 
-SUMMARY_SIZE = 5
+st.subheader("Index Sentence Ranking")
+col3, col4 = st.beta_columns([3, 1])
+ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
+col3.write(ranked_sentence_indexes)
+SUMMARY_SIZE = st.slider("Berapa Jumlah Size?", 0, 3, 5)
+# SUMMARY_SIZE = 5
 selected_sentences = sorted(ranked_sentence_indexes[:SUMMARY_SIZE])
-st.write(selected_sentences)
+col4.write(selected_sentences)
 
 st.subheader("Summary Result")
 summary = itemgetter(*selected_sentences)(sentences)
