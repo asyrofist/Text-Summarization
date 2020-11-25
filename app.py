59e228271bcb971af72b95f09c7c6c0770333495
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-25 09:11:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,17 +81,17 @@ col1.write(hasil_disambiguation)
 sentence_ranks = pagerank(hasil_disambiguation)
 col2.write(sentence_ranks)
 
-## Load Word Sense Disambiguation 
-# st.subheader("Index Sentence Ranking")
-# col3, col4 = st.beta_columns([3, 1])
-# ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
-# col3.dataframe(ranked_sentence_indexes)
-# SUMMARY_SIZE = st.slider("Berapa Jumlah Size?", 0, 10, 5)
-# # SUMMARY_SIZE = 5
-# selected_sentences = sorted(ranked_sentence_indexes[:SUMMARY_SIZE])
-# col4.dataframe(selected_sentences)
+# Load Word Sense Disambiguation 
+st.subheader("Index Sentence Ranking")
+col3, col4 = st.beta_columns([3, 1])
+ranked_sentence_indexes = [item[0] for item in sorted(enumerate(sentence_ranks), key=lambda item: -item[1])]
+col3.dataframe(ranked_sentence_indexes)
+SUMMARY_SIZE = st.slider("Berapa Jumlah Size?", 0, 10, 5)
+# SUMMARY_SIZE = 5
+selected_sentences = sorted(ranked_sentence_indexes[:SUMMARY_SIZE])
+col4.dataframe(selected_sentences)
 
-# st.subheader("Summary Result")
-# summary = itemgetter(*selected_sentences)(sentences)
-# for sent in summary:
-#     st.write(' '.join(sent))
+st.subheader("Summary Result")
+summary = itemgetter(*selected_sentences)(sentences)
+for sent in summary:
+    st.write(' '.join(sent))
