f87d978e22bbd3a452c79ecb3f7bd11171cf7b32
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-25 16:13:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ elif genre == 'wordembed':
     st.sidebar.subheader("Word2vec Parameter")
     size_value = st.sidebar.slider("Berapa size?", 0, 1000, 100)
     mode_value = st.sidebar.selectbox("Pilih Mode", [1, 0])
-    windows_value = st.sidebar.slider("WIndows Size?", 0, 10, 3)
+    window_value = st.sidebar.slider("WIndows Size?", 0, 10, 3)
     iteration_value = st.sidebar.slider("iteration size?", 0, 100, 10) 
     word2vec_model = Word2Vec(sentences = sentences, size = size_value, sg = mode_value, window = window_value, min_count = 1, iter = iteration_value, workers = Pool()._processes)
     word2vec_model.init_sims(replace = True)
