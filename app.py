6d35985d890efa00ca7815502c6821e417f706cb
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-25 16:31:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,8 @@ elif genre == 'wordembed':
     iteration_value = st.sidebar.slider("iteration size?", 0, 100, 10) 
     word2vec_model = Word2Vec(sentences = sentences, size = size_value, sg = mode_value, window = window_value, min_count = 1, iter = iteration_value, workers = Pool()._processes)
     word2vec_model.init_sims(replace = True)
+    embedd_vectors = word2vec_model.wv.vectors
+    st.write(embedd_vectors)
 #     embedd_vectors = word2vec_model.vectors
     unknown_embedd = np.zeros(300)
     hasil = word_embedding(sentences)
