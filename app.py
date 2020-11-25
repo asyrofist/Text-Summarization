fd246354d235cae9b1299e3393043214b128e449
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-25 09:50:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,6 +158,7 @@ elif genre == 'Disambiguation':
     closest, _ = pairwise_distances_argmin_min(modelmn.cluster_centers_, vector)
     ordering = sorted(range(n_clusters), key=lambda k: avg[k])
 #     summary = ' '.join([sentences[closest[idx]] for idx in ordering])
-    st.write([sentences[closest[idx]] for idx in ordering])
+    summary = [sentences[closest[idx]] for idx in ordering]
+    st.text(summary)
     
 
