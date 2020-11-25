8ebe893039238b392251abfd2d37afbe11a13bac
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-25 14:14:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ nltk.download('brown')
 
 st.header("Summarization Corpus Brown")
 st.subheader("Dataset parameter")
-st.sidebar.slider("Berapa Dataset", 0, len(brown.fileids()), 10)
-dataset = st.sidebar.selectbox("Choose Brown Dataset?", brown.fileids())
+banyak_data = st.sidebar.slider("Berapa Dataset", 0, len(brown.fileids()), 10)
+dataset = st.sidebar.selectbox("Choose Brown Dataset?", brown.fileids()[:len(banyak_data)])
 sentences = brown.sents(dataset)
 list_sentences = [' '.join(sent) for sent in sentences]
 st.dataframe(list_sentences)
