43db3a0db31a2c5d230c8efef5d01372e78fcc72
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-27 21:00:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import pairwise_distances_argmin_min
 from gensim.models import Word2Vec
 from multiprocessing import Pool
-from nltk.corpus import wordnet as wn
 from pywsd.cosine import cosine_similarity
 
 nltk.download('brown')
