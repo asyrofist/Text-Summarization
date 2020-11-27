3fa68ab2b585370f0f80762391042dcb2b5377fd
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-27 17:06:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ elif genre == 'compareMethod':
     
     data = """The Fulton County Grand Jury said Friday an investigation of Atlanta's recent primary election produced no evidence '' that any irregularities took place . The Fulton County Grand Jury said Friday an investigation of Atlanta's recent primary election produced no evidence '' that any irregularities took place ."""
     
-    reference = data' # actual
+    reference = data # actual
     rouge = Rouge()
     scores = rouge.get_scores(hypothesis, reference)
     st.write(scores)
