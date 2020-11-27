5ad21e5cb7efb8051d2f3c929a27a457625b91b2
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-27 19:25:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ elif genre == 'compareMethod':
     SUMMARY_SIZE = st.sidebar.slider("Berapa Jumlah Cluster?", 1, len(word_embedding(sentences)), 44)
     avg = []
     n = SUMMARY_SIZE
-    vector = [word_embedding(sentences[i]) for i in range(len(list_sentences))]
+    vector = [word_embedding(sentences[i]) for i in range(len(sentences))]
     n_clusters = len(sentences)//n
     modelmn = MiniBatchKMeans(n_clusters=n_clusters) #minibatch
     modelmn = modelmn.fit(vector)
