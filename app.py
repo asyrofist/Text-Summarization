08283d112f709a499bfa37a591bd521dfef947c3
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-27 18:27:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -222,8 +222,8 @@ elif genre == 'compareMethod':
 #     col6.dataframe(ordering)
 
     st.subheader("Summary Word2vecCluster Result")
-    summary = ' '.join([list_sentences[closest[idx]] for idx in ordering])
-    st.write(summary)
+    ringkasan = ' '.join([list_sentences[closest[idx]] for idx in ordering])
+    st.write(ringkasan)
     
     # Sentence Ranking
 #     st.subheader("Sentence Ranking")
@@ -253,7 +253,7 @@ elif genre == 'compareMethod':
     from rouge import Rouge 
 #     hypothesis = "the #### transcript is a written version of each day 's cnn student news program use this transcript to he    lp students with reading comprehension and vocabulary use the weekly newsquiz to test your knowledge of storie s you     saw on cnn student news"
 #     reference = "this page includes the show transcript use the transcript to help students with reading comprehension and     vocabulary at the bottom of the page , comment for a chance to be mentioned on cnn student news . you must be a teac    her or a student age # # or older to request a mention on the cnn student news roll call . the weekly newsquiz tests     students ' knowledge of even ts in the news"
-    hypothesis = (hasilSummary)
+    hypothesis = (ringkasan)
     reference = (hasilSummary)
     rouge = Rouge()
     scores = rouge.get_scores(hypothesis, reference)
