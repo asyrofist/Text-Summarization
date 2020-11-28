bab74adfda907ee02832a94bccc59369e8d9d377
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-28 07:22:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import pairwise_distances_argmin_min
 from gensim.models import Word2Vec
 from multiprocessing import Pool
+import pywsd
 
 nltk.download('brown')
 
