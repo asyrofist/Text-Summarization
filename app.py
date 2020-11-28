221870600dc50dd23109c053025001f9ed37155b
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-29 06:32:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ elif genre == 'disambiguationRank':
     st.subheader("Summary Result")
     summary = itemgetter(*selected_sentences)(list_sentences)
 #     hasilSummary = [' '.join(sent) for sent in summary]
-    hasilSummary = ' '.join(summary)
+    hasilSummary = (summary)
     st.write(hasilSummary)
 #     for sent in summary:
 #         st.write(' '.join(sent))        
