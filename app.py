d6bea3273a66665a884b4851d4c9126fe78dd60d
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-29 07:04:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ elif genre == 'disambiguationCluster':
 
     hasil_disambiguation = pd.DataFrame(disambiguation_df)
     st.dataframe(hasil_disambiguation)
-    vector = int(hasil_disambiguation)
+    vector = disambiguation_df
     SUMMARY_SIZE = st.sidebar.slider("Berapa Jumlah Cluster?", 1, len(sentences), 20)
     n = SUMMARY_SIZE
     avg = []
