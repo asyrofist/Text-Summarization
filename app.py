2992b5a4755ee2eeea908d1c5d3fcb85f0c055b2
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-30 08:32:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -344,11 +344,12 @@ elif genre == 'wordembedCluster':
     st.write(summary)
     
 elif genre == 'validation':  
-    pilihanGroundtruth = st.selectbox("Pilih Dataset?", text_dataset['Ground Truth'])
+    pilihanGroundtruth = st.selectbox("Pilih Dataset?", text_dataset['Ground Truth']])
+    st.write(pilihanGroundtruth)
     st.subheader("Hypothesis")
-    message1 = st.text_area("Enter your Text", height=200, value = pilihanGroundTruth, key="kalimat1")
+    message1 = st.text_area("Enter your Text", height=200, value = "Tulis Text", key="kalimat1")
     st.subheader("Reference")
-    message2 = st.text_area("Enter your Text", height=200, value = pilihanGroundTruth, key="kalimat2")
+    message2 = st.text_area("Enter your Text", height=200, value = "Tulis Text", key="kalimat2")
     # penilaian rouge
     hypothesis = (message1)
     reference = (message2)
